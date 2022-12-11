567fab78e85ee27b4a9c798de9449fd6e771d62f
vkashegde <vikas@kofluence.com>
2022-12-11 23:34:41 +0530

    categories added

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,14 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:provydr/services/utils.dart';
+import 'package:provydr/widgets/categories_widget.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({Key? key}) : super(key: key);
+  CategoriesScreen({Key? key}) : super(key: key);
+
+  List<Color> gridColors = [
+    const Color(0xff53B175),
+    const Color(0xffF8A44C),
+    const Color(0xffF7A593),
+    const Color(0xffD3B0E0),
+    const Color(0xffFDE598),
+    const Color(0xffB7DFF5),
+  ];
+
+  List<Map<String, dynamic>> catInfo = [
+    {
+      'imgPath': 'assets/images/cat/fruits.png',
+      'catText': 'Fruits',
+    },
+    {
+      'imgPath': 'assets/images/cat/veg.png',
+      'catText': 'Vegetables',
+    },
+    {
+      'imgPath': 'assets/images/cat/Spinach.png',
+      'catText': 'Herbs',
+    },
+    {
+      'imgPath': 'assets/images/cat/nuts.png',
+      'catText': 'Nuts',
+    },
+    {
+      'imgPath': 'assets/images/cat/spices.png',
+      'catText': 'Spices',
+    },
+    {
+      'imgPath': 'assets/images/cat/grains.png',
+      'catText': 'Grains',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
+    final utils = Utils(context);
     return Scaffold(
-      body: Center(
-        child: Text('Categories'),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          elevation: 0,
+          title: Text(
+            'Categories',
+            style: TextStyle(color: utils.color),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.count(
+            crossAxisCount: 2,
+            childAspectRatio: 240 / 250,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            children: List.generate(6, (index) {
+              return CategoriesWidget(
+                catText: catInfo[index]['catText'],
+                imgPath: catInfo[index]['imgPath'],
+                gridColor: gridColors[index],
+              );
+            }),
+          ),
+        ));
   }
 }
